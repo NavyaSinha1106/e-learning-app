a3b50414d4677bf548f28b380f2fce95ee900742
Navya Sinha <navyasinha1118@gmail.com>
2022-09-22 19:59:35 +0530

    minor code fixes

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -8,14 +8,23 @@ function HomePage(props) {
     <>
       <TopNav onAddToCart={props.onAddToCart} count={props.count}></TopNav>
       <div className="home-container">
-        <p className="home-text">
-          Learn new skills online<br></br> with top educators
-          <Search />
-        </p>
-        <img
-          className="image"
-          src="https://international.humber.ca/assets/images/Home/globalsystemschallenge.jpg"
-        ></img>
+        <div className="search-text">
+          <div className="home-text-container">
+            <p className="home-text">
+              Learn new skills online<br></br> with top educators
+            </p>
+          </div>
+          <div className="search-component">
+            <Search />
+          </div>
+        </div>
+
+        <div className="home-image-container">
+          <img
+            className="image"
+            src="https://international.humber.ca/assets/images/Home/globalsystemschallenge.jpg"
+          ></img>
+        </div>
       </div>
       <div className="home-info">
         <h3>A large selection of courses</h3>
